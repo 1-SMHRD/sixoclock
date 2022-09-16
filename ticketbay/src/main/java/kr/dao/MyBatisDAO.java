9eaf7bb6b02fc0619494fc458dc0030926a24445
smhrd <smhrd@DESKTOP-0SIFSM3>
2022-09-16 10:21:43 +0900

    template

---
+++ ticketbay/src/main/java/kr/dao/MyBatisDAO.java
@@ -0,0 +1,24 @@
+package kr.dao;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MyBatisDAO {
+	private static SqlSessionFactory sqlSessionFactory;
+	// 초기화 블럭
+	static {
+		try {
+			String resource = "kr/dao/config.xml";
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ ticketbay/src/main/java/kr/frontcontroller/ViewResolver.java
@@ -0,0 +1,8 @@
+package kr.frontcontroller;
+
+public class ViewResolver {
+	public static String makeView(String view) {
+		// 수정해야함
+		return "board/"+view+".jsp";
+	}
+}
